66cab14a10bd944df46e24a062d7da3006e2055f
agung udijana <agung.udijana@gmail.com>
2015-11-10 02:25:20 +0100

    updated order of methods, so the methods are in the C.R.U.D. order ;-)

--- Oprdacht2A/src/BestellingDao.java
+++ Oprdacht2A/src/BestellingDao.java
@@ -3,8 +3,8 @@ import java.util.List;
 public interface BestellingDao {
 
 	public void create(Bestelling bestelling_id);
-	public void update(Bestelling bestelling);
 	public List<Bestelling> read();
+	public void update(Bestelling bestelling);
 	public void delete(Bestelling bestelling);
 
 }
