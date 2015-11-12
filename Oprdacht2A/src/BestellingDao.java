beee75ca9e4aeb0a6273130afe2764a576be27ef
agungudijana <agung.udijana@gmail.com>
2015-11-12 04:09:44 +0100

    update methode aangepast
    getest. werkt goed

--- Oprdacht2A/src/BestellingDao.java
+++ Oprdacht2A/src/BestellingDao.java
@@ -4,7 +4,6 @@ public interface BestellingDao {
 
 	public void create(Bestelling bestelling);
 	public List<Bestelling> read();
-	public void update(Bestelling bestelling);
+	public void update(int bestelling_id, int new_artikel_id, String new_artikel_naam, int new_artikel_aantal, double new_artikel_prijs);
 	public void delete(int bestelling_id);
-
 }
