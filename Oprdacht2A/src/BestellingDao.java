4bcfbec91a1d429bf4f68b4ae2a3893bc30c9fef
agungudijana <agung.udijana@gmail.com>
2015-11-13 02:00:28 +0100

    Bestelling object als parameter ipv primitieve waardes

--- Oprdacht2A/src/BestellingDao.java
+++ Oprdacht2A/src/BestellingDao.java
@@ -4,6 +4,8 @@ public interface BestellingDao {
 
 	public void create(Bestelling bestelling);
 	public List<Bestelling> read();
-	public void update(int bestelling_id, int new_artikel_id, String new_artikel_naam, int new_artikel_aantal, double new_artikel_prijs);
-	public void delete(int bestelling_id);
+	public void update(Bestelling bestelling);
+	// oude versie : public void update(int bestelling_id, int new_artikel_id, String new_artikel_naam, int new_artikel_aantal, double new_artikel_prijs);
+	public void delete (Bestelling bestelling);
+	// oude versie : public void delete(int bestelling_id);
 }
