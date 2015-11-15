eaf48cd309617e1d8fcc1c2e48720607ad09f596
agungudijana <agung.udijana@gmail.com>
2015-11-15 23:03:58 +0100

    returntypes create,update,delete method veranderd naar Int
    conform veranderingen in BestellingDaoImpl.java

--- Oprdacht2A/src/BestellingDao.java
+++ Oprdacht2A/src/BestellingDao.java
@@ -2,10 +2,10 @@ import java.util.List;
 
 public interface BestellingDao {
 
-	public void create(Bestelling bestelling);
+	public int create(Bestelling bestelling);
 	public List<Bestelling> read();
-	public void update(Bestelling bestelling);
+	public int update(Bestelling bestelling);
 	// oude versie : public void update(int bestelling_id, int new_artikel_id, String new_artikel_naam, int new_artikel_aantal, double new_artikel_prijs);
-	public void delete (Bestelling bestelling);
+	public int delete (Bestelling bestelling);
 	// oude versie : public void delete(int bestelling_id);
 }
