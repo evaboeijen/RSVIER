4d147d46004671513bf4bef9a20a2070ffcef834
agung udijana <agung.udijana@gmail.com>
2015-11-16 16:37:58 +0100

    Suite klasse voor de jUnit testklasses

---
+++ Oprdacht2A/src/AllJunitTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BestellingDaoImpl_jUnitTest.class	 })  // later nog meer jUnit test klasses toevoegen
+public class AllJunitTests {
+
+} 
