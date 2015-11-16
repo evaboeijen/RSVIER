3cd76cf3a83ae7959688aa3bda856d0d1a0af473
agung udijana <agung.udijana@gmail.com>
2015-11-16 16:42:15 +0100

    klasse naam aangepast

--- Oprdacht2A/src/BestellingDaoImpl_jUnitTest.java
+++ Oprdacht2A/src/BestellingDaoImpl_jUnitTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class BestellingDaoImplTest {
+public class BestellingDaoImpl_jUnitTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
