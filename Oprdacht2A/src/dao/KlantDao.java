1c67d0d704730ed2a2c9dc6eb51e6d025e4569eb
JvdBemt <JvdBemt@users.noreply.github.com>
2015-11-17 18:45:45 +0100

    Update and rename Oprdacht2A/src/KlantDao.java to Oprdacht2A/src/dao/KlantDao.java

--- Oprdacht2A/src/dao/KlantDao.java
+++ Oprdacht2A/src/dao/KlantDao.java
@@ -1,13 +1,16 @@
+package dao;
 import java.util.*;
 
 
 public interface KlantDao {
    
 public List<Klant> read();   
-public Klant getKlant(int klant_id);
-public Klant getKlant(String voornaam);
+public Klant readKlant(int klant_id);
+public Klant readKlant(String voornaam);
 public void create(Klant klant);   
 public void update(Klant klant);   
 public void delete(Klant klant);
 }
+
+
 
