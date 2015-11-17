90ae43dec2ad28f8f4fdc5ecf2d840672f2ebf5b
JvdBemt <JvdBemt@users.noreply.github.com>
2015-11-17 18:52:10 +0100

    Update and rename Oprdacht2A/src/ArtikelDao.java to Oprdacht2A/src/dao/ArtikelDao.java

--- Oprdacht2A/src/dao/ArtikelDao.java
+++ Oprdacht2A/src/dao/ArtikelDao.java
@@ -1,9 +1,11 @@
+package dao;
+
 import java.util.*;
 
 public interface ArtikelDao {
    
 public List<Artikel> read();   
-public Artikel getArtikel(int artikel_id);
+public Artikel readArtikel(int artikel_id);
 public void create(Artikel artikel);   
 public void update(Artikel artikel);   
 public void delete(Artikel artikel);
