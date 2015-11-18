9b87f26dd63f377d0224700961aef25c7964fdb2
JvdBemt <JvdBemt@users.noreply.github.com>
2015-11-18 11:44:21 +0100

    1 testIT methode van gemaakt

--- Oprdacht2A/src/junittests/KlantDaoImpl_jUnitTest.java
+++ Oprdacht2A/src/junittests/KlantDaoImpl_jUnitTest.java
@@ -5,28 +5,33 @@ import dao.*;
 
 import static org.junit.Assert.*;
 
-import org.junit.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
 import org.junit.Test;
 
 
 public class KlantDaoImpl_jUnitTest {
 
 
-	static KlantDaoImpl testKlantDaoImpl = new KlantDaoImpl(); 
-	Klant testKlant = new Klant();
 	
-	@BeforeClass
-	public static void testInitializeDB(){
-		
-		testKlantDaoImpl.initializeDB();		
-		assertNotNull("Connection should not be null", testKlantDaoImpl.connection );
-		
-	}
 	 
 	@Test
-	public void testCreate(){
+	public void testIT(){
 		
-		testKlant.setKlant_id(4102);
+		KlantDaoImpl testKlantDaoImpl = new KlantDaoImpl(); 
+		
+		
+		// testConnection method	
+		testKlantDaoImpl.initializeDB();		
+		assertNotNull("Connection should not be null", testKlantDaoImpl.connection );
+			
+		
+		// test create and readKlant(String) method
+		Klant testKlant = new Klant();
+		testKlant.setKlant_id(1);
 		testKlant.setVoornaam("jUnit");
 		testKlant.setAchternaam("Test");
 		testKlant.setEmail("jUnitTest@email.com");
@@ -35,38 +40,40 @@ public class KlantDaoImpl_jUnitTest {
 		
 		assertEquals("Object hashcode should be the same", testKlant.hashCode(), klant.hashCode());
 		
-	}
-	
-	@Test
-	public void testRead(){
-		
-		
-		assertEquals("Number of rows is 3", 3 , testKlantDaoImpl.read().size());
-		
-	}
-		
-	@Test
-	public void testReadKlant(){
-		Klant klant = testKlantDaoImpl.readKlant(4102);
-		assertEquals("HashCode should be equal", testKlant, klant);
-	}
 
+		// test read method
+		Klant testKlant2 = new Klant();
+		testKlant2.setKlant_id(1869);
+		testKlant2.setVoornaam("Klant");
+		testKlant2.setTussenvoegsel("has been");
+		testKlant2.setAchternaam("Made");
+		testKlant2.setEmail("@email.com");
+		testKlantDaoImpl.create(testKlant2);
+		
+		List<Klant> klantList = new LinkedList<Klant>();
+		klantList.add(testKlant);
+		klantList.add(testKlant2);
+		
+		System.out.println(testKlantDaoImpl.read());
+		
+		assertEquals("ArrayLists must be the same", klantList.hashCode(),testKlantDaoImpl.read().hashCode());
 		
 	
-		@Test
-		public void testDelete(){
-		
-			testKlant.setKlant_id(4102);
+		// test delete and readKlant(int) method
 			testKlantDaoImpl.delete(testKlant);
-			Klant klant = testKlantDaoImpl.readKlant(4102);
-			assertEquals("Klant id should be 0", 0 , klant.getKlant_id());
-			assertEquals("Klant voornaam should be null", null, klant.getVoornaam());
-			assertEquals("Klant achternaam should be null", null, klant.getAchternaam());
-			assertEquals("Klant tussenvoegsel should be null", null, klant.getTussenvoegsel());
-			assertEquals("Klant email should be null", null, klant.getEmail());
-			
-		}
+			Klant klant2 = testKlantDaoImpl.readKlant(1);
+			assertEquals("Klant id should be 0", 0 , klant2.getKlant_id());
+			assertEquals("Klant voornaam should be null", null, klant2.getVoornaam());
+			assertEquals("Klant achternaam should be null", null, klant2.getAchternaam());
+			assertEquals("Klant tussenvoegsel should be null", null, klant2.getTussenvoegsel());
+			assertEquals("Klant email should be null", null, klant2.getEmail());
+			testKlantDaoImpl.delete(testKlant2);
 		
+	
+		 testKlantDaoImpl.closeDBConnection();		
+		assertNull("Connection should be null again", testKlantDaoImpl.connection);
+	}
+	
 		@Test
 		public void testEquals() {	
 			KlantDaoImpl testKlantDaoImpl1 = new KlantDaoImpl(); 
@@ -75,12 +82,6 @@ public class KlantDaoImpl_jUnitTest {
 		}
 
 	
-		@AfterClass
-		public static void testCloseDBConnection() {
-		
-			 testKlantDaoImpl.closeDBConnection();		
-			assertNull("Connection should be null again", testKlantDaoImpl.connection);
-		}
 		
 
 }
