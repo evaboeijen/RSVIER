e6dcdd3025256d5d30db4b48dd985b117d0fa86c
agung udijana <agung.udijana@gmail.com>
2015-11-18 12:23:46 +0100

    updated the jUnit test suite

--- Oprdacht2A/src/junittests/AllJunitTests.java
+++ Oprdacht2A/src/junittests/AllJunitTests.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BestellingDaoImpl_jUnitTest.class	 })  // later nog meer jUnit test klasses toevoegen
+@SuiteClasses({ BestellingDaoImpl_jUnitTest.class, KlantDaoImpl_jUnitTest.class	 })  // later test unit voor Artikel en Adres toevoegen
 public class AllJunitTests {
 
 } 
