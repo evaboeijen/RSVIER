f9573f7a593dcbececa0b1a78710fe0b54c7d668
agungudijana <agung.udijana@gmail.com>
2015-11-23 21:51:20 +0100

    Update DeleteMenu.java

--- Oprdacht2A/src/menu/crud/DeleteMenu.java
+++ Oprdacht2A/src/menu/crud/DeleteMenu.java
@@ -62,6 +62,11 @@ public class DeleteMenu  {
                 			Adres teWissenAdres = new Adres();
                 			
                 			teWissenAdres.setKlant_id(gewensteKlant_id);
+                			teWissenAdres.setHuisnummer(0);
+                			teWissenAdres.setPostcode("-");
+                			teWissenAdres.setStraatnaam("-");
+                			teWissenAdres.setToevoeging("-");
+                			teWissenAdres.setWoonplaats("-");
                 	               			
                 			adresDaoImpl.deleteAdres(teWissenAdres);
          			}
@@ -74,19 +79,9 @@ public class DeleteMenu  {
             			           		          		
                 
             	case 2:          		
- 
-            			         		         		
-            		break;
-            		
-                
-            	case 3:
-            		System.out.println();           		
-            		System.out.println("Je kan hier complete bestellingen verwijderen");
-            		System.out.println("Hieronder een overzicht van alle bestellingen: ");
-            		
-               		BestellingDaoImpl bestellingDaoImpl4 = new BestellingDaoImpl();
+            		BestellingDaoImpl bestellingDaoImpl2 = new BestellingDaoImpl();
         			
-            		List<Bestelling> lijst4 = bestellingDaoImpl4.read();	// lees en toon alle bestellingen uit de Bestelling tabel
+            		List<Bestelling> lijst2 = bestellingDaoImpl2.read();	// lees en toon alle bestellingen uit de Bestelling tabel
             		
             		System.out.println();
             		System.out.println();	
@@ -94,7 +89,75 @@ public class DeleteMenu  {
             		System.out.println("Overzicht van alle bestellingen: ");
             		System.out.println("=================================");
             		
-            		for (Bestelling overzicht : lijst4) {
+            		for (Bestelling overzicht : lijst2) {
+            			System.out.println("Klantnummer : " + overzicht.getKlant_id() + ". Ordernummer : " + overzicht.getBestelling_id());
+            			System.out.println("---------------------------------------------");
+            			System.out.println("Artikelnummer: " + overzicht.getArtikel1_id() + ". Artikelnaam: " + overzicht.getArtikel1_naam() + ". Aantal: "+ overzicht.getArtikel1_aantal() + ". Prijs: " + overzicht.getArtikel1_prijs());
+            			System.out.println("Artikelnummer: " + overzicht.getArtikel2_id() + ". Artikelnaam: " + overzicht.getArtikel2_naam() + ". Aantal: "+ overzicht.getArtikel2_aantal() + ". Prijs: " + overzicht.getArtikel2_prijs());
+            			System.out.println("Artikelnummer: " + overzicht.getArtikel3_id() + ". Artikelnaam: " + overzicht.getArtikel3_naam() + ". Aantal: "+ overzicht.getArtikel3_aantal() + ". Prijs: " + overzicht.getArtikel3_prijs());
+            			System.out.println();
+            			System.out.println();	
+            		}
+            			   
+            		System.out.println();          		
+            		System.out.print("Voer het bestelling ID in van de bestelling waaruit je het artikel wil verwijderen: ");
+            		System.out.println();          		
+
+            		
+            		try (Scanner input2 = new Scanner(System.in);) {
+        				int gewensteBestelling_id = input.nextInt();
+        				
+        				BestellingDaoImpl bestellingDaoImpl = new BestellingDaoImpl();
+            			while (bestellingDaoImpl.checkBestelling_id(gewensteBestelling_id)!= true) { 
+            				System.out.print("\nVoer een ander bestellingnummer in: ");
+            				gewensteBestelling_id = input.nextInt();
+            				System.out.println();
+            			}      
+            		
+            			          			               		
+            			
+            			System.out.println();   
+                		System.out.print("Voer het artikel ID in: ");
+                		System.out.println();  
+                	
+            			try (Scanner input3 = new Scanner(System.in);) {
+        					int gewensteArtikel_id = input.nextInt();
+        				
+        					BestellingDaoImpl bestellingDaoImpl3 = new BestellingDaoImpl();
+    						while (bestellingDaoImpl3.checkArtikel_id(gewensteArtikel_id)!= true) { 
+    							System.out.print("\nVoer een ander artikelummer in: ");
+    							gewensteArtikel_id = input.nextInt();
+    							System.out.println();
+    						}     
+    					
+    					
+    						ArtikelDaoImpl artikelDaoImpl = new ArtikelDaoImpl();
+                    	
+            					artikelDaoImpl.delete(gewensteBestelling_id, gewensteArtikel_id);
+            			
+            			}
+            		
+            		}
+        
+            		break;
+            		
+         
+            	case 3:
+            		System.out.println();           		
+            		System.out.println("Je kan hier complete bestellingen verwijderen");
+            		System.out.println("Hieronder een overzicht van alle bestellingen: ");
+            		
+               		BestellingDaoImpl bestellingDaoImpl3 = new BestellingDaoImpl();
+        			
+            		List<Bestelling> lijst3 = bestellingDaoImpl3.read();	// lees en toon alle bestellingen uit de Bestelling tabel
+            		
+            		System.out.println();
+            		System.out.println();	
+            		
+            		System.out.println("Overzicht van alle bestellingen: ");
+            		System.out.println("=================================");
+            		
+            		for (Bestelling overzicht : lijst3) {
             			System.out.println("Klantnummer : " + overzicht.getKlant_id() + ". Ordernummer : " + overzicht.getBestelling_id());
             			System.out.println("---------------------------------------------");
             			System.out.println("Artikelnummer: " + overzicht.getArtikel1_id() + ". Artikelnaam: " + overzicht.getArtikel1_naam() + ". Aantal: "+ overzicht.getArtikel1_aantal() + ". Prijs: " + overzicht.getArtikel1_prijs());
@@ -127,7 +190,52 @@ public class DeleteMenu  {
             		}
                		break; 
                 	
-            	case 4:
+            	case 4:           		
+            	    System.out.println();
+            		KlantDaoImpl lijst4 = new KlantDaoImpl();
+            		System.out.println("Hieronder een overzicht van alle klanten: ");
+            		
+            		List <Klant> alleKlanten2 = lijst4.read();
+               		
+            		
+            		System.out.println();          		
+            		System.out.print("Voer het klant ID van de klant die je wil wissen: ");
+            		System.out.println(); 
+            			                      		
+            			try (Scanner input2 = new Scanner(System.in);) {
+            				int gewensteKlant_id = input.nextInt();
+            				
+            				AdresDaoImpl adresDaoImpl = new AdresDaoImpl();
+                			while (adresDaoImpl.checkKlant_id(gewensteKlant_id)!= true) { 
+                				System.out.print("\nVoer een ander klantnummer in: ");
+                				gewensteKlant_id = input.nextInt();
+                				System.out.println();
+                			}    
+                			
+                			
+                			KlantDaoImpl klantDaoImpl = new KlantDaoImpl();
+                			Klant teWissenKlant = new Klant();
+                			
+                			teWissenKlant.setKlant_id(gewensteKlant_id);
+            
+                			klantDaoImpl.delete(teWissenKlant);
+         			}
+            		    
+            			
+                		catch (Exception ex)	 {
+                			ex.printStackTrace();                			
+                		}
+            		
+            		
+            		
+            		
+            		
+            		
+            		
+            		
+            		
+            		
+            		
             		break;
             		
             
@@ -150,11 +258,13 @@ public class DeleteMenu  {
             		
             	default:
             		System.out.println("\n! Ongeldige optie, probeer het nogmaals !\n");
-            		this.toonMenu();
+        			DeleteMenu deletemenu = new DeleteMenu();
+            		deletemenu.toonMenu();
 			} 
 			
-    		CrudMenu crudmenu = new CrudMenu();
-    		crudmenu.toonMenu();
+    		input.close();
+			DeleteMenu deletemenu = new DeleteMenu();
+    		deletemenu.toonMenu();
         
 		}
 		
