8de97e640db86802bdc40e918742cee85bc6197e
JvdBemt <JvdBemt@users.noreply.github.com>
2015-11-24 23:48:00 +0100

    read methode aangepast

--- Oprdacht2A/src/dao/ArtikelDao.java
+++ Oprdacht2A/src/dao/ArtikelDao.java
@@ -1,15 +1,14 @@
 package dao;
 
-import java.util.*;
-
 import business.Artikel;
 
 public interface ArtikelDao {
    
-public List<Artikel> read();   
+public Artikel read(Artikel artikel);   
 public Artikel readArtikel(int bestelling_id, int artikel_id);
 public void create(Artikel artikel);   
 public void update(Artikel artikel);   
 public void delete(int bestelling_id, int artikel_id);
 
 }
+
