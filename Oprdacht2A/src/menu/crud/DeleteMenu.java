c1fcf8850a2d8edb2eaeaca9083ed81201ee32c0
kuija <kuija@LT130433.ws.hva.nl>
2015-11-29 20:36:06 +0100

    - DeleteMenu almost done

--- Oprdacht2A/src/menu/crud/DeleteMenu.java
+++ Oprdacht2A/src/menu/crud/DeleteMenu.java
@@ -1,10 +1,15 @@
 package menu.crud;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 import dao.*;
-import menu.HoofdMenu;
+import menu.*;
 import business.*;
+
 
 public class DeleteMenu  {
 	
@@ -22,6 +27,11 @@ public class DeleteMenu  {
 	int gewensteBestelling_id = 0;
 	ArtikelDaoImpl artikelDaoImpl = new ArtikelDaoImpl();
 	Klant teWissenKlant = new Klant();
+	int gewensteKlantID = 0;
+	int gewensteArtikel_id = 0;
+	String gewensteArtikelNaam = null;
+	int gewensteArtikelPrijs = 0;
+	int gewensteAantal = 0;
 	
 	
 	public void toonMenu() {
@@ -30,8 +40,9 @@ public class DeleteMenu  {
 	    System.out.println("\t-------------");
 	    System.out.println("1. Delete het adres van een bestaande klant");  
 	    System.out.println("2. Delete een artikel van een bestaande bestelling");  
-	    System.out.println("3. Delete een bestaande bestelling");  
-	    System.out.println("4. Delete een klant");  		 
+	    System.out.println("3. Delete een bestaande bestelling");
+	    System.out.println("4. Delete een artikel uit het assortiment");
+	    System.out.println("5. Delete een klant");  		 
 	    System.out.println();	    
 	    System.out.println("10. Terug naar het vorige menu");
 	    System.out.println("11. Terug naar het hoofdmenu"); 
@@ -42,6 +53,7 @@ public class DeleteMenu  {
             
 		int keuze = input.nextInt();
 		       
+		
 		switch (keuze) {
 		
         	case 1:
@@ -83,53 +95,131 @@ public class DeleteMenu  {
                 
             case 2:          		
             		       			
-            	lijst = bestellingDaoImpl.read();	// lees en toon alle bestellingen uit de Bestelling tabel
-            		
-            	System.out.println();
-            	System.out.println();	
-            		
-            	System.out.println("Overzicht van alle bestellingen: ");
-            	System.out.println("=================================");
-            		
-            	for (Bestelling overzicht : lijst) {
-            		System.out.println("Klantnummer : " + overzicht.getKlant_id() + ". Ordernummer : " + overzicht.getBestelling_id());
-            		System.out.println("---------------------------------------------");
-            		System.out.println("Artikelnummer: " + overzicht.getArtikel1_id() + ". Artikelnaam: " + overzicht.getArtikel1_naam() + ". Aantal: "+ overzicht.getArtikel1_aantal() + ". Prijs: " + overzicht.getArtikel1_prijs());
-            		System.out.println("Artikelnummer: " + overzicht.getArtikel2_id() + ". Artikelnaam: " + overzicht.getArtikel2_naam() + ". Aantal: "+ overzicht.getArtikel2_aantal() + ". Prijs: " + overzicht.getArtikel2_prijs());
-            		System.out.println("Artikelnummer: " + overzicht.getArtikel3_id() + ". Artikelnaam: " + overzicht.getArtikel3_naam() + ". Aantal: "+ overzicht.getArtikel3_aantal() + ". Prijs: " + overzicht.getArtikel3_prijs());
-            		System.out.println();
-            		System.out.println();	
-            	}
-            			   
-            	System.out.println();          		
-            	System.out.print("Voer het bestelling ID in van de bestelling waaruit je het artikel wil verwijderen: ");
-            	System.out.println();          		
-
-        		gewensteBestelling_id = input.nextInt();
-        				
+            	System.out.println();           		
+        		System.out.println("Je kan hier artikelen verwijderen van een bestaande bestelling.");
+        		System.out.println("Hieronder een overzicht van alle bestellingen en hun artikelen: ");
+        		               		       			
+        		lijst = bestellingDaoImpl.read();	// lees en toon alle bestellingen uit de Bestelling tabel
         		
-            	while (bestellingDaoImpl.checkBestelling_id(gewensteBestelling_id)!= true) { 
-            		System.out.print("\nVoer een ander bestellingnummer in: ");
-            		gewensteBestelling_id = input.nextInt();
-            		System.out.println();
-            	}      
-            		
-            	System.out.println();   
-                System.out.print("Voer het artikel ID in: ");
-                System.out.println();  
+        		System.out.println();
+        		System.out.println();	
+        		
+        		System.out.println("Overzicht van alle bestellingen: ");
+        		System.out.println("=================================");
+        		
+        		for (Bestelling overzicht : lijst) {
+        			System.out.println("Klantnummer : " + overzicht.getKlant_id() + ". Ordernummer : " + overzicht.getBestelling_id());
+        			System.out.println("---------------------------------------------");
+        			
+        			//aanpassing tbv opdracht 5 || AU 26/11/15
+        			System.out.println("Artikelnummer: " + overzicht.getArtikel_id() + ". Artikelnaam: " + overzicht.getArtikel_naam() + ". Aantal: "+ overzicht.getArtikel_aantal() + ". Prijs: " + overzicht.getArtikel_prijs());
+        			System.out.println();
+        			System.out.println();	
+        		}
+        		
+        		System.out.println();          		
+        		System.out.print("Voer het bestelling ID in van de bestelling waarvan je artikelen wil verwijderen: ");
+        		System.out.println(); 
+        		            		           		
+    			gewensteBestelling_id = input.nextInt();
+    				
+
+        		while (bestellingDaoImpl.checkBestelling_id(gewensteBestelling_id)!= true) { 
+        			System.out.print("\nVoer een ander bestellingnummer in: ");
+        			gewensteBestelling_id = input.nextInt();
+        			System.out.println();
+        		}            		        
+        		
+        		try {
+        			Connection connection = DBConnectivityManagement.getConnectionStatus();
                 	
-        		int gewensteArtikel_id = input.nextInt();
-        							
-    			while (bestellingDaoImpl.checkArtikel_id(gewensteArtikel_id)!= true) { 
-    				System.out.print("\nVoer een ander artikelummer in: ");
-    				gewensteArtikel_id = input.nextInt();
-    				System.out.println();
-    			}     
-    					
-    			artikelDaoImpl.delete(gewensteBestelling_id, gewensteArtikel_id);
-            			
-            	toonMenu();
-            	break;
+        			PreparedStatement preparedStatement;
+        			preparedStatement = connection.prepareStatement("SELECT Klant_id FROM Bestelling WHERE Bestelling_id = ?");
+        			preparedStatement.setInt(1, gewensteBestelling_id);   
+        			ResultSet resultSet = preparedStatement.executeQuery();
+        			
+        			while(resultSet.next()){                                
+        				gewensteKlantID = resultSet.getInt("Klant_id");                                                                                                    
+        			}
+        			resultSet.close();
+        			preparedStatement.close();
+                         
+        		} 
+        		
+        		catch (SQLException e) {
+        			e.printStackTrace();
+        		}
+        		
+
+        		System.out.print("\nVoer het artikel ID in dat je wil verwijderen van de bestelling (0 is terug naar het vorige menu): ");
+        		System.out.println(); 
+        		gewensteArtikel_id = input.nextInt();
+        		
+        		while (bestellingDaoImpl.checkArtikelAlAanwezigInBestelling(gewensteBestelling_id, gewensteArtikel_id) != true) {
+        			System.out.print("\nArtikelnummer niet gevonden in desbetreffende bestelling. Voer een ander artikelnummer in: ");
+                	gewensteArtikel_id = input.nextInt();
+                	if (gewensteArtikel_id == 0) {
+                		toonMenu();
+                	}
+                	System.out.println();
+        		}
+        		
+        		/* while (bestellingDaoImpl.checkArtikel_id(gewensteArtikel_id)!= true) { 
+        			System.out.print("\nVoer een ander artikelnummer in: ");
+                    gewensteArtikel_id = input.nextInt();
+                    System.out.println();
+                }    */
+        		
+	
+        		try {
+
+        			Connection connection = DBConnectivityManagement.getConnectionStatus();
+        					
+        			// uitgecomment tbv opdracht 5 | 27/11/15 |AU            		
+        			// PreparedStatement statement = connection.prepareStatement("SELECT ARTIKEL1_NAAM, ARTIKEl1_PRIJS FROM Bestelling WHERE Artikel1_id=?");
+        			// nieuw tbv opdracht 5 | 27/11/15 |AU     
+        			PreparedStatement statement = connection.prepareStatement("SELECT ARTIKEL_NAAM, ARTIKEL_PRIJS FROM artikel JOIN bestelling_artikel WHERE (bestelling_artikel.artikel_id = artikel.artikel_id AND artikel.artikel_id=?)");
+        			statement.setInt(1, gewensteArtikel_id);
+
+        			ResultSet resultSet = statement.executeQuery();
+        				     		
+        			
+					
+					while(resultSet.next()){          
+        				// aangepast tbv opdracht 5 | 27/11/15 |AU     
+        				gewensteArtikelNaam = resultSet.getString("Artikel_naam");
+        				gewensteArtikelPrijs = resultSet.getInt("Artikel_prijs");
+        			}
+        			      
+        			//System.out.println("AANTEPASSENBESTELLING OBJECT");	debug statement
+        			
+        			
+					Bestelling aantepassenBestelling = new Bestelling(gewensteBestelling_id, gewensteKlantID, gewensteArtikel_id, gewensteArtikelNaam, gewensteAantal , gewensteArtikelPrijs);
+        				
+        			
+        			//System.out.println(aantepassenBestelling.getBestelling_id());   debug statements
+        			//System.out.println(aantepassenBestelling.getKlant_id());
+        			//System.out.println(aantepassenBestelling.getArtikel_id());
+        			//System.out.println(aantepassenBestelling.getArtikel_naam());
+        			//System.out.println(aantepassenBestelling.getArtikel_aantal());
+        			//System.out.println(aantepassenBestelling.getArtikel_prijs());
+        			
+        			// System.out.println("UPDATE METHODE WORDT AANGEROEPEN"); debug statements
+
+        			          		
+        			bestellingDaoImpl.deleteArtikelFromBestelling(aantepassenBestelling);  
+        		}
+        			
+        		catch (SQLException e) {
+        			e.printStackTrace();
+        		}
+        			
+        		finally {
+        			// zinnige code
+        		}
+
+            	toonMenu();          			   			  		          		
+            	break; 	
             		
             	
          
@@ -147,15 +237,15 @@ public class DeleteMenu  {
             	System.out.println("Overzicht van alle bestellingen: ");
             	System.out.println("=================================");
             		
-            	for (Bestelling overzicht : lijst) {
-            		System.out.println("Klantnummer : " + overzicht.getKlant_id() + ". Ordernummer : " + overzicht.getBestelling_id());
-            		System.out.println("---------------------------------------------");
-            		System.out.println("Artikelnummer: " + overzicht.getArtikel1_id() + ". Artikelnaam: " + overzicht.getArtikel1_naam() + ". Aantal: "+ overzicht.getArtikel1_aantal() + ". Prijs: " + overzicht.getArtikel1_prijs());
-            		System.out.println("Artikelnummer: " + overzicht.getArtikel2_id() + ". Artikelnaam: " + overzicht.getArtikel2_naam() + ". Aantal: "+ overzicht.getArtikel2_aantal() + ". Prijs: " + overzicht.getArtikel2_prijs());
-            		System.out.println("Artikelnummer: " + overzicht.getArtikel3_id() + ". Artikelnaam: " + overzicht.getArtikel3_naam() + ". Aantal: "+ overzicht.getArtikel3_aantal() + ". Prijs: " + overzicht.getArtikel3_prijs());
-            		System.out.println();
-            		System.out.println();	
-            	}
+        		for (Bestelling overzicht : lijst) {
+        			System.out.println("Klantnummer : " + overzicht.getKlant_id() + ". Ordernummer : " + overzicht.getBestelling_id());
+        			System.out.println("---------------------------------------------");
+        			
+        			//aanpassing tbv opdracht 5 || AU 26/11/15
+        			System.out.println("Artikelnummer: " + overzicht.getArtikel_id() + ". Artikelnaam: " + overzicht.getArtikel_naam() + ". Aantal: "+ overzicht.getArtikel_aantal() + ". Prijs: " + overzicht.getArtikel_prijs());
+        			System.out.println();
+        			System.out.println();	
+        		}
             	
             	System.out.println();          		
             	System.out.print("Voer het bestelling ID in van de bestelling die je wil verwijderen: ");
@@ -177,8 +267,12 @@ public class DeleteMenu  {
             	toonMenu();
             	break; 
                 	
+            case 4:	
+            	System.out.println("Hier komt een nieuwe methode om artikelen uit het assortiment te verwijderen");   
+            	toonMenu();
+            	break; 
             	
-            case 4:           		
+            case 5:           		
             	System.out.println();          
             	System.out.println("Hieronder een overzicht van alle klanten: ");
             		
