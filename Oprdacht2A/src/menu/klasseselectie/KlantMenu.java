094ddebc8c0e8e4626ffe58ee65e274390fdc398
JvdBemt <JvdBemt@users.noreply.github.com>
2015-12-07 21:14:51 +0100

    logging

--- Oprdacht2A/src/menu/klasseselectie/KlantMenu.java
+++ Oprdacht2A/src/menu/klasseselectie/KlantMenu.java
@@ -1,192 +1,295 @@
+package dao;
 
-	package menu.klasseselectie;
+import java.sql.*;
+import java.util.*;
+
+import business.Klant;
+import menu.DBConnectivityManagement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+	
+
+
+public class KlantDaoImpl implements KlantDao {
 
 	
-	import java.sql.*;
-	import java.util.*;
-	import dao.*;
-	import menu.*;
-	import business.*;
+	private static final Logger logger =  LoggerFactory.getLogger(KlantDaoImpl.class);
+	
+	public Connection connection = null;
 
-	public class KlantMenu  {
-		
-		
-		
-		public Klant createKlantObject() {
-			
-			Scanner input = new Scanner(System.in);					
-			
-		    System.out.println("Vul uw gegevens in. Voer in uw voornaam, en druk op enter: ");
-			String voornaam = input.nextLine();
-			
-					while (voornaam.length() > 50){
-						System.out.println("Een voornaam mag niet meer dan 50 karakters bevatten! \n Voornaam: ");
-						voornaam = input.nextLine();
-					}
-					
-			System.out.println("Tussenvoegsel: ");
-				String tussenvoegsel = input.nextLine();
-					while (tussenvoegsel.length() > 12){
-					System.out.println("Een tussenvoegsel niet meer dan 12 karakters bevatten! \n Tussenvoegsel: ");
-						tussenvoegsel = input.nextLine();
-					}			
-			System.out.println("Achternaam: ");
-				String achternaam = input.nextLine();
-					while (achternaam.length() > 51){
-					System.out.println("Een achternaam mag niet meer dan 51 karakters bevatten! \n Achternaam: ");
-						achternaam = input.nextLine();
-					}
-			System.out.println("Emailadres: ");
-				String email =  input.nextLine();
-					while (email.length() > 80){
-					System.out.println("Een email mag niet meer dan 80 karakters bevatten! \n Emailadres: ");
-						email = input.nextLine();
-					}
-			
-					Klant klant = new Klant();
-					klant.setKlant_id(klant.getKlant_id());
-					klant.setVoornaam(voornaam);
-					klant.setTussenvoegsel(tussenvoegsel);
-					klant.setAchternaam(achternaam);
-					klant.setEmail(email);
-					
-					return klant;
-					}		
-		
-				
-		
-		public void toonMenu() {
-		    System.out.println("\t-----------");
-		    System.out.println("\tKlant Menu");
-		    System.out.println("\t-----------");
-		    System.out.println("1. Maak een nieuwe Klant id aan");  
-		    System.out.println("2. Lees uw klantgegevens");  
-		    System.out.println("3. Wijzig uw bestaande klantgegevens");  
-		    System.out.println("4. Wis uw klant id en uw gegevens");  		 
-		    System.out.println();	    
-		    System.out.println("10. Terug naar het vorige menu");
-		    System.out.println("11. Terug naar het hoofdmenu"); 
-		    System.out.println("12. Stoppen"); 
-		    System.out.println();
-		    System.out.print("Voer optie in en druk op Enter:");
-		    
-		   Scanner input = new Scanner(System.in);		     
-	            
-				int keuze = input.nextInt();
-			       
-				switch (keuze) {
-	            	case 1:	
-	            				KlantDaoImpl nieuweKlantDaoImpl = new KlantDaoImpl();
-	            				Klant nieuweKlant = createKlantObject();
-	            				nieuweKlantDaoImpl.create(nieuweKlant);
-	            				System.out.println();
-	            				System.out.println("Een nieuwe klant is aangemaakt");
-	            				nieuweKlantDaoImpl.read();
-	            				
-	            				toonMenu();
-	            		break; 
-	            			           		          		
-	                
-	            	case 2:          		
-	            			            	          			
-	            				KlantDaoImpl leesKlantDaoImpl = new KlantDaoImpl();
-	            				
-	            				System.out.println();
-	            				System.out.println("Voer uw klant id in, en druk op enter: ");
-	            				System.out.println();
-	            				
-	            			    int huidige_klant_id = input.nextInt();
-	    	        				
-	    	        				
-	    	            			while (leesKlantDaoImpl.checkKlant_id(huidige_klant_id)!= true) { 
-	    	            				System.out.print("\n Incorrecte invoer. Voer uw klant id opnieuw in: ");
-	    	            				huidige_klant_id = input.nextInt();
-	    	            				System.out.println();
-	    	            			}            		
-	            				
-	            				System.out.println("Uw huidige gegevens zijn: ");
-	            				leesKlantDaoImpl.readKlant(huidige_klant_id);
-	            				toonMenu();
-	            		break;
-	            		
-	                
-	            	case 3:
-	            				KlantDaoImpl updateKlantDaoImpl = new KlantDaoImpl();
-        				
-	            				System.out.println();
-	            				System.out.println("Voer uw klant id in, en druk op enter: ");
-	            				System.out.println();
-        				
-	            			
-	            				huidige_klant_id = input.nextInt();
-	        				
-	        				
-	            				while (updateKlantDaoImpl.checkKlant_id(huidige_klant_id)!= true) { 
-	            					System.out.print("\n Incorrecte invoer. Voer uw klant id opnieuw in: ");
-	            					huidige_klant_id = input.nextInt();
-	            					System.out.println();
-		            			}            		
-	        				
-	            				            				
-	            			    Klant updateKlant = createKlantObject();
-	            			    updateKlant.setKlant_id(huidige_klant_id);
-	            				
-	            				updateKlantDaoImpl.update(updateKlant);
-	            				
-	            				System.out.println("Uw gegevens zijn aanpast");
-	            				
-	            				toonMenu();
-	            		break;
-	            				
-	            		
-	            	case 4:
-	            		
-	            			KlantDaoImpl verwijderKlantDaoImpl = new KlantDaoImpl();
-	            		
-	            			System.out.println();           		       		
-	            			System.out.print("Voer het klant ID in die u wilt verwijderen: ");
-	            			System.out.println(); 
 
-	            		
-	            			huidige_klant_id = input.nextInt();
-    				
-    				
-	            			while (verwijderKlantDaoImpl.checkKlant_id(huidige_klant_id)!= true) { 
-	        					System.out.print("\n Incorrecte invoer. Voer uw klant id opnieuw in: ");
-	        					huidige_klant_id = input.nextInt();
-	        					System.out.println();
-	            			}            		
-    				        			
-	            			Klant verwijderKlant = new Klant();
-	            			verwijderKlant.setKlant_id(huidige_klant_id);
-	            		
-	            			verwijderKlantDaoImpl.delete(verwijderKlant);
-	            			
-	            			toonMenu();
-	            			break;
-	            
-	            		
-	            	case 10:
-	            		KlasseSelectieMenu klasseselectiemenu = new KlasseSelectieMenu();
-	            		klasseselectiemenu.toonMenu();
-	            		break;
-	            		
-	            	case 11:
-	            		HoofdMenu hoofdmenu = new HoofdMenu();					
-	            		hoofdmenu.toonMenu(); 
-	            		break;
-	            		
-	            	case 12:
-	            		System.out.println("\nTot de volgende keer...");
-	            		System.exit(1);
-	            		break;
-	            		
-	            		
-	            	default:
-	            		System.out.println("\n! Ongeldige optie, probeer het nogmaals !\n");
-	            		this.toonMenu();
-				} 
-				
-		}
+   public Connection initializeDB(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            if(connection == null){ 
+                String dbURL = "jdbc:mysql://localhost:3308/opdracht1";
+                String username = "root";
+                String password = "JaRsvier15";
+
+        
+                connection = DriverManager.getConnection(dbURL, username, password);
+                  System.out.println("Verbinding is gemaakt");
+            }
+ 
+        } catch (ClassNotFoundException e) {
+ 
+            e.printStackTrace();
+             
+        } catch (SQLException e) {
+             
+            e.printStackTrace();
+             
+        }
+      
+        return connection;
+        
+    }
+    
+   @Override
+    public void create(Klant klant){
+        
+	logger.info("Create Klant methode start");
+                   
+        try {
+        	   Connection connection = DBConnectivityManagement.getConnectionStatus();
+                        
+            	PreparedStatement preparedStatement = connection.prepareStatement("insert into klant(klant_id, voornaam, achternaam, tussenvoegsel, email) values (?, ?, ?, ? , ?)");
+			preparedStatement.setInt(1, klant.getKlant_id());
+			preparedStatement.setString(2, klant.getVoornaam());
+			preparedStatement.setString(3, klant.getAchternaam());
+			preparedStatement.setString(4, klant.getTussenvoegsel());
+			preparedStatement.setString(5, klant.getEmail());
+            
+                     
+            
+            int rowsInserted = preparedStatement.executeUpdate();
+            
+            if (rowsInserted > 0) {
+            System.out.println("Een nieuwe klant is succesvol toegevoegd aan de database");
+        }
+            
+            logger.info("create Klant methode: een nieuwe klant " + klant.getVoornaam() +  
+            		" " + klant.getTussenvoegsel() + " " + klant.getAchternaam() + " " + klant.getEmail() + " is toegevoegd aan de database");   
+            logger.info("object klant", klant.getKlant_id());
+            
+            preparedStatement.close();
+            
+            
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+        	logger.warn("SQL exception bij create klant methode");
+            e.printStackTrace();
+        }
+       
+        logger.info("Create klant methode eindigt");
+    }
+ 
+   @Override
+    public List<Klant> read() {
+	   logger.info("read all Klanten methode start");
+	   
+        List<Klant> klanten = new ArrayList<Klant>();      
+        
+        try {
+                Connection connection = DBConnectivityManagement.getConnectionStatus();
+        		
+        		Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM klant");
+                 
+                Klant klant;
+                while(resultSet.next()){
+                    klant = new Klant();
+                    klant.setKlant_id(resultSet.getInt("Klant_id"));
+                    klant.setVoornaam(resultSet.getString("voornaam"));
+                    klant.setTussenvoegsel(resultSet.getString("tussenvoegsel"));
+                    klant.setAchternaam(resultSet.getString("achternaam"));
+                    klant.setEmail(resultSet.getString("email"));
+                                        
+                    
+                    klanten.add(klant);
+                }
+                resultSet.close();
+                statement.close();
+                 
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        
+            System.out.println(klanten);
+            logger.info("read all Klanten methode eindigt");
+            return klanten;
+    }
+    
+    @Override
+    public Klant readKlant(int klant_id) {
+	logger.info("readKlant(int klant_id) methode start voor klant_id: " + klant_id);
+        
+        Klant klant = new Klant();
+        ResultSet result;
+        String sql = "SELECT * FROM klant WHERE Klant_id= " + klant_id;
+        
+        try {
+        	Connection connection = DBConnectivityManagement.getConnectionStatus();
+        	
+            Statement statement = connection.createStatement();
+            result = statement.executeQuery(sql);
+            
+            
+             while (result.next()){
+                klant.setKlant_id(klant_id);
+                klant.setVoornaam(result.getString(2));
+                klant.setTussenvoegsel(result.getString(4));
+                klant.setAchternaam(result.getString(3));
+                klant.setEmail(result.getString(5));
+              
+                
+        }
+             result.close();
+             statement.close();
+                     
+        } catch (SQLException e){
+        	logger.warn("SQL Exception voor readKlant(int klant_id) methode"); 
+            e.printStackTrace();
+           
+        }
+       System.out.println(klant);
+       logger.info("readKlant(int klant_id) methode voor klant_id: " + klant_id + " is beeindigd");
+       return klant;
+}
+ 
+    @Override
+ public Klant readKlant(String voornaam) {
+    	logger.info("readKlant(String voornaam) methode start voor voornaam: " + voornaam);
+        
+        Klant klant = null;
+        ResultSet result;
+                
+        try {
+        	   Connection connection = DBConnectivityManagement.getConnectionStatus();
+        	   
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM klant WHERE voornaam=?");
+            statement.setString(1, voornaam);
+            result = statement.executeQuery();
+            
+             while (result.next()){
+                klant = new Klant();
+                klant.setKlant_id(result.getInt(1));
+                klant.setVoornaam(result.getString(2));
+                klant.setTussenvoegsel(result.getString(4));
+                klant.setAchternaam(result.getString(3));
+                klant.setEmail(result.getString(5));
+              
+                
+        }
+             result.close();
+             statement.close();
+             
+        } catch (SQLException e){
+        	logger.info("SQL Exception voor readKlant(String voornaam)");
+            e.printStackTrace();
+           
+        }
+       System.out.println(klant);
+       logger.info("readKlant(String voornaam) methode start voor voornaam: " + voornaam);
+       return klant;
+}
+        
+	@Override
+	public void update(Klant klant) {
+		logger.info("update Klant methode start"); 
+                                    
+            try {
+            	Connection connection = DBConnectivityManagement.getConnectionStatus();
+
+                PreparedStatement statement = connection.prepareStatement("UPDATE klant SET voornaam=?, tussenvoegsel=?, achternaam=?, email=? WHERE Klant_id=?");
+                statement.setString(1, klant.getVoornaam());
+                statement.setString(2, klant.getTussenvoegsel());
+                statement.setString(3, klant.getAchternaam());
+                statement.setString(4, klant.getEmail());
+                statement.setInt(5, klant.getKlant_id());
+               
+
+                int rowsUpdated = statement.executeUpdate();
+                if (rowsUpdated > 0) {
+	System.out.println("De gegevens van de bestaande klant zijn aangepast!");
+}
+             logger.info("update klant methode. De gegevens van de volgende klant id " + klant.getKlant_id() + " zijn geupdate naar naam:" + klant.getVoornaam() +  
+             		" " + klant.getTussenvoegsel() + " " + klant.getAchternaam() + " met email: " + klant.getEmail());   
+             statement.close();
+             
+            } catch (SQLException e) {
+            	logger.warn("SQL Exception voor update Klant methode");
+                e.printStackTrace();
+            } 
+            logger.info("update Klant methode eindigt");
 	}
-	
+
+        @Override
+	public void delete(Klant klant) {
+		// TODO Auto-generated method stub
+    logger.info("delete Klant methode start");     	
+        	
+            try{ 
+            	   Connection connection = DBConnectivityManagement.getConnectionStatus();
+                          
+                PreparedStatement statement = connection.prepareStatement("DELETE FROM Klant WHERE Klant_id=?");
+                statement.setInt(1, klant.getKlant_id());
+
+                int rowsDeleted = statement.executeUpdate();
+                if (rowsDeleted > 0) {
+	System.out.println("De klant is gewist uit de database");
+        }
+                logger.info("delete klant methode. De gegevens van de klant met klant id: " + klant.getKlant_id() + " zijn gewist uit de database");
+                statement.close();
+                
+	} catch (SQLException e) {
+			logger.warn("SQL Exception voor delete klant methode");
+            e.printStackTrace();
+        }
+        }
+
+    
+    	public boolean checkKlant_id(int klant_id) {	
+    		logger.info("checkKlant_id methode start");
+    		
+			PreparedStatement preparedStatement;
+			ResultSet resultSet;
+			boolean result = false;
+			
+			try {
+				
+				Connection connection = DBConnectivityManagement.getConnectionStatus(); 
+				
+				preparedStatement = connection.prepareStatement("SELECT * FROM Klant WHERE klant_id=?");
+				preparedStatement.setInt(1, klant_id);
+				resultSet = preparedStatement.executeQuery(); 
+				
+				if (resultSet.next()){
+					result = true;
+				} else {
+					System.out.println("Het opgegeven klant_id bevindt zich niet in de database...");
+				}
+
+			} catch (SQLException e) {
+				logger.warn("SQL Exception voor checkKlant_id methode");
+	                e.printStackTrace();	
+			}
+			logger.info("checkKlant_id methode eindigt");
+			return result;
+		}
+    	
+     
+    public void closeDBConnection(){
+        try {
+              if (connection != null) {
+                  connection.close();
+                  System.out.println("Verbinding is gesloten");
+              }
+            } catch (Exception e) { 
+                //do nothing
+            }
+    }
+}
