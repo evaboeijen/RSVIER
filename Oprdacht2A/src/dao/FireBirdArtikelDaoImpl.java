11fad0251d4d35694642a04a199fec8bb1e29b3a
JvdBemt <jbemt@hotmail.com>
2015-12-15 11:45:51 +0100

    Klant, artikel Firebird + MySQL

---
+++ Oprdacht2A/src/dao/FireBirdArtikelDaoImpl.java
@@ -0,0 +1,52 @@
+package dao;
+
+import java.util.*;
+
+import business.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FireBirdArtikelDaoImpl extends ArtikelDaoImpl {
+	
+	private static final Logger logger =  LoggerFactory.getLogger(KlantDaoImpl.class);
+	
+	
+	@Override
+	public List<Artikel> read(){
+	
+		List<Artikel> artikellen = super.read();
+		return artikellen;
+	
+	}
+	
+	@Override
+	public Artikel read(Artikel artikel){
+		return super.read(artikel);
+	}
+	
+	@Override
+	public Artikel readArtikel(int bestelling_id, int artikel_id){
+		return super.readArtikel(bestelling_id, artikel_id);
+	}
+	
+	@Override
+	public void create(Artikel artikel){
+		super.create(artikel);
+	}
+	
+	@Override
+	public void update(Artikel artikel){
+		super.update(artikel);
+	}
+	
+	@Override
+	public void delete(int bestelling_id, int artikel_id){
+		super.delete(bestelling_id, artikel_id);
+	}
+	
+	@Override
+	public void delete(Artikel artikel){
+		super.delete(artikel);
+	}
+}
---
+++ Oprdacht2A/src/dao/FireBirdKlantDaoImpl.java
@@ -0,0 +1,50 @@
+package dao;
+
+
+import java.util.*;
+
+import business.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FireBirdKlantDaoImpl extends KlantDaoImpl {
+
+	private static final Logger logger =  LoggerFactory.getLogger(KlantDaoImpl.class);
+	
+	
+	@Override
+    public void create(Klant klant){
+		super.create(klant);
+	}
+	
+	 @Override
+	    public List<Klant> read() {
+
+		 List<Klant> klanten = super.read();
+		 return klanten;
+	 }
+	
+	 @Override
+	 public Klant readKlant(int klant_id){
+		 Klant klant = super.readKlant(klant_id);
+		 return klant;
+	 }
+	 
+	 @Override
+	 public Klant readKlant(String voornaam){
+		 Klant klant = super.readKlant(voornaam);
+		 return klant;
+	 }
+	 
+	 @Override   
+	 public void update(Klant klant){
+		 super.update(klant);
+	 }
+	 
+	 @Override
+	 public void delete(Klant klant){
+		 super.delete(klant);
+	 }
+}
+
