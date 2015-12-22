730610d033e3c5527227e540dce65d5b86613afb
agung udijana <agung.udijana@gmail.com>
2015-12-22 11:48:09 +0100

    annotations toegevoegd

---
+++ Oprdacht2A/src/annotations/Entity.java
@@ -0,0 +1,16 @@
+package annotations;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Entity {
+	
+	String entityName();
+
+	//zinnige code
+	
+	
+
+}
